c56db587204b4b9e451db162e2aebeda743e0317
dodytrifa <dody.trifa@gmail.com>
2021-05-25 22:14:40 +0700

    debug setup

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
-var a = "Hello this is typescript";
-console.log(a);
+// let a: string; = "Hello this is typescript version 2";
+// console.log(a)
+//*tentukan tipe datanya
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("John", "Doe"));
--- test.ts
+++ test.ts
@@ -1,2 +1,9 @@
-let a = "Hello this is typescript version 2";
+let a : string = "Hello this is typescript version 2";
 console.log(a)
+
+//*tentukan tipe datanya
+// const getFullName(name : string, surname: string) => {
+//   return name + " " + surname;
+// }
+
+// console.log(getFullName("John", "Doe"));
